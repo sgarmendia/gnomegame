c19c0fabfd67d60ef7ada8996e34b16c4a68562a
Simon Garmendia <sgarmendia@gmail.com>
2018-08-27 22:08:53 +0200

    defining App component test suite

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,30 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import mockAPI from './Api/__mocks__/BrastlewarkAPI';
+import { shallow, mount } from 'enzyme';
 import App from './App';
+
 
 it('shallow renders correctly', () => {
   shallow(<App />);
 });
+
+it('mounts correctly with all subcomponents', () => {
+  mount(<App />);
+});
+
+it('render error message if API fails',() => {
+  const wrapper = shallow(<App />)
+  wrapper.setState({ error: true })
+  wrapper.containsMatchingElement(<h2 />)
+  expect(wrapper.contains(<h2>Oops, something went wrong!</h2>)).toEqual(true)
+})
+
+jest.mock('./Api/__mocks__/BrastlewarkAPI')
+
+it('calls the API correctly', async () => {
+  const data = await mockAPI()
+  expect(data).toHaveLength //recieved data is an array
+  expect(mockAPI).toHaveBeenCalledTimes(1) //API is called 1 time
+  expect(mockAPI).toHaveBeenCalledWith() //API is called with no parameters
+})
+
