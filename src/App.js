f17ab53f8c70730597f02b6529c8301caf76b496
Simon Garmendia <sgarmendia@gmail.com>
2018-08-27 12:14:14 +0200

    setup advanced filter

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ class App extends Component {
     this.state = {
       data: [],
       gnomes: [],
+      age: [],
+      weight: [],
+      height: [],
+      hairColor: [],
       selectedGnome: {},
       overlay: false,
       error: null,
@@ -31,13 +35,6 @@ class App extends Component {
     }
   }
 
-  
-  handleSearch = e => {
-    const gnomes = this.state.data.filter(g => g.name.toLowerCase().includes(e.target.value.toLowerCase()))
-    this.setState({ gnomes });
-
-  }
-
   handleClick = e => {
     const selectedGnome = this.state.data.find(g => g.id === +e.target.id)
     this.setState({ selectedGnome },() => {
@@ -49,11 +46,66 @@ class App extends Component {
     this.setState({ overlay: false, selectedGnome: {} });
   }
 
-  handleSlide = (value,att) => {
-    const attribute = att.toLowerCase()
-    const gnomes = this.state.data.filter(g => +g[attribute] <= +value)
-    this.setState({ gnomes });
+  handleSearch = e => {
+    const search = this.state.data.filter(g => g.name.toLowerCase().includes(e.target.value.toLowerCase()))
+    this.setState({ search },() => {
+      this.globalFilter()
+    });
+  }
+
+  handleSlide = ([ min, max ],att) => {
+    switch (att) {
+      case 'age':
+        const age = this.state.data.filter(g => +g[att] >= min && +g[att] <= max)
+        this.setState({ age });
+        break;
+      case 'weight':
+        const weight = this.state.data.filter(g => +g[att] >= min && +g[att] <= max)
+        this.setState({ weight });
+        break;
+      case 'height':
+        const height = this.state.data.filter(g => +g[att] >= min && +g[att] <= max)
+        this.setState({ height });
+        break;
+      default:
+        break;
+    }
+    this.globalFilter()
   } 
+
+  colorFilter = (color) => {
+    const hairColor = this.state.data.filter(g => g.hair_color === color)
+    this.setState({ hairColor },() => {
+      this.globalFilter()
+    });
+  }
+
+  globalFilter = () => {
+    const searchSet = new Set(this.state.search ? this.state.search : this.state.data)
+    const ageSet = new Set(this.state.age.length === 0 ? this.state.data : this.state.age)
+    const weightSet = new Set(this.state.weight.length === 0 ? this.state.data : this.state.weight)
+    const heightSet = new Set(this.state.height.length === 0 ? this.state.data : this.state.height)
+    const hairSet = new Set(this.state.hairColor.length === 0 ? this.state.data : this.state.hairColor)
+    const intersection = new Set(
+      [...searchSet]
+        .filter(g => ageSet.has(g))
+        .filter(g => weightSet.has(g))
+        .filter(g => heightSet.has(g))
+        .filter(g => hairSet.has(g))
+    )
+    const gnomes = [...intersection]
+    this.setState({ gnomes });
+  }
+
+  clearSearch = () => {
+    this.setState({
+      gnomes: this.state.data,
+      age: [],
+      weight: [],
+      height: [],
+      hairColor: [],
+    });
+  }
 
   componentWillUnmount() {
     this.mounted = false
@@ -67,6 +119,8 @@ class App extends Component {
           data={this.state.data}
           search={this.handleSearch}
           slide={this.handleSlide}
+          color={this.colorFilter}
+          clear={this.clearSearch}
         />
         {this.state.error
           ? <h2>Oops, something went wrong!</h2>
