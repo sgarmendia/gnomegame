f12ccfe8c634b3dc44f4e653350ab4faba43ce7b
Simon Garmendia <sgarmendia@gmail.com>
2018-08-27 12:14:36 +0200

    refactor

--- src/components/Gnomes.js
+++ src/components/Gnomes.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import '../css/Gnomes.css'
+import orc from '../css/Assets/orc.png';
 
 
 class Photos extends Component {
   render() {
-    return (
-      <div className='gnomeContainer'>
-        {this.props.data && this.props.data.map(gnome => {
+    return (this.props.data.length === 0 
+    ? <div className='emptygnomes'>
+          <img src={orc} alt={'No gnomes'} />
+      </div>
+    : <div className='gnomeContainer'>
+        {this.props.data.map(gnome => {
           return (
             <div key={gnome.id} className='imgContainer'>
                 <img src={gnome.thumbnail} alt={gnome.name}
